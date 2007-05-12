24a78e2daf66f21a43007ef7ad5fccd556f3611d
Simon Huerlimann <simon.huerlimann@huerlisi.ch>
2007-05-12 12:48:45 +0000

    indentation fixes
    svn path=/trunk/KDE/kdegames/kspaceduel/; revision=663842

--- mainview.cpp
+++ mainview.cpp
@@ -1,7 +1,8 @@
-/* Copyright (C) 1998-2001 Andreas Zehender <az@azweb.de>
-   Copyright (C) 2006-2007 Dirk Rathlev <dirkrathlev@gmx.de>
+/*
+    Copyright (C) 1998-2001 Andreas Zehender <az@azweb.de>
+    Copyright (C) 2006-2007 Dirk Rathlev <dirkrathlev@gmx.de>
 
-This program is free software; you can redistribute it and/or modify
+    This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation; either version 2 of the License, or
     (at your option) any later version.
