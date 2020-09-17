36648233ccd09558d9c292e3752e31d7cfd0dbeb
Friedrich W. H. Kossebau <kossebau@kde.org>
2020-09-17 03:31:34 +0200

    No more need for I18N_NOOP string juggling
    GIT_SILENT

--- main.cpp
+++ main.cpp
@@ -27,8 +27,6 @@
 #include "kspaceduel_version.h"
 #include "topwidget.h"
 
-static const char description[] = I18N_NOOP("KDE Space Game");
-
 int main(int argc,char **argv)
 {
     // Fixes blurry icons with fractional scaling
@@ -39,7 +37,8 @@ int main(int argc,char **argv)
 
     KAboutData aboutData( QStringLiteral("kspaceduel"), i18n("KSpaceDuel"), 
             QStringLiteral(KSPACEDUEL_VERSION_STRING),
-            i18n(description), KAboutLicense::GPL,
+            i18n("Space Arcade Game"),
+            KAboutLicense::GPL,
             i18n("(c) 1998-2001, Andreas Zehender"),  QStringLiteral("https://kde.org/applications/games/org.kde.kspaceduel") );
     aboutData.addAuthor(i18n("Andreas Zehender"),i18n("Original Program"), QStringLiteral("az@azweb.de"));
     aboutData.addAuthor(i18n("Branan Riley"),i18n("SVG Renderer"), QStringLiteral("branan@gmail.com"));
